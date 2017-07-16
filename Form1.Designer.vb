63789998c51197498799463223f100ebec905fbe
haxtivitiez <gh0stsync.rt@gmail.com>
2017-07-16 08:18:03 +0700

    add files
    add files

---
+++ Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(158, 68)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(271, 24)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(158, 148)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(271, 24)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(158, 274)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 32)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "คำนวณ"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 72)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = " อุณหภูมิแห้ง:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 148)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 18)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "อุณหภูมิเปียก:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 223)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 18)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "ผลลัพธ์"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(158, 220)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(271, 24)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(319, 281)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 18)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "ผลต่าง:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(372, 281)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 18)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "-"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(48, 346)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(101, 18)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Developed by:"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(155, 346)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(275, 18)
+        Me.LinkLabel1.TabIndex = 10
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "https://www.facebook.com/un4ckn0wl3z"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(512, 373)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Humidity Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim dry As Integer
+        Dim wet As Integer
+        Dim diff As Integer
+        Dim result As Integer
+
+        Dim DataLists()() = New Integer(40)() {}
+        DataLists(0) = {80, 60, 40, 21, 3, 0, 0}
+        DataLists(1) = {81, 62, 43, 25, 8, 0, 0}
+        DataLists(2) = {82, 64, 46, 29, 12, 0, 0}
+        DataLists(3) = {82, 65, 49, 32, 16, 1, 0}
+        DataLists(4) = {83, 67, 51, 35, 20, 5, 0}
+        DataLists(5) = {84, 68, 53, 38, 24, 9, 0}
+        DataLists(6) = {85, 70, 55, 41, 27, 13, 0}
+        DataLists(7) = {85, 71, 57, 43, 30, 17, 4}
+        DataLists(8) = {86, 72, 59, 46, 33, 20, 8}
+        DataLists(9) = {86, 73, 60, 48, 36, 24, 12}
+        DataLists(10) = {87, 74, 62, 50, 38, 27, 16}
+        DataLists(11) = {87, 75, 63, 52, 40, 29, 19}
+        DataLists(12) = {88, 76, 65, 53, 43, 32, 23}
+        DataLists(13) = {88, 77, 65, 55, 45, 34, 25}
+        DataLists(14) = {89, 78, 67, 57, 46, 37, 27}
+        DataLists(15) = {89, 78, 68, 58, 48, 39, 30}
+        DataLists(16) = {89, 79, 69, 59, 50, 41, 32}
+        DataLists(17) = {90, 80, 70, 61, 51, 43, 34}
+        DataLists(18) = {90, 80, 71, 62, 53, 44, 36}
+        DataLists(19) = {90, 81, 72, 63, 54, 46, 38}
+        DataLists(20) = {91, 81, 73, 64, 56, 48, 40}
+        DataLists(21) = {91, 82, 73, 65, 57, 49, 42}
+        DataLists(22) = {91, 82, 74, 66, 58, 50, 43}
+        DataLists(23) = {91, 83, 75, 67, 59, 52, 44}
+        DataLists(24) = {91, 83, 75, 68, 60, 53, 46}
+        DataLists(25) = {92, 84, 76, 68, 61, 54, 47}
+        DataLists(26) = {92, 84, 76, 69, 62, 55, 49}
+        DataLists(27) = {92, 84, 77, 70, 63, 56, 50}
+        DataLists(28) = {92, 85, 77, 70, 64, 57, 51}
+        DataLists(29) = {92, 85, 78, 71, 64, 58, 52}
+        DataLists(30) = {92, 85, 78, 72, 65, 59, 53}
+        DataLists(31) = {93, 86, 79, 72, 66, 60, 54}
+        DataLists(32) = {93, 86, 79, 73, 66, 61, 55}
+        DataLists(33) = {93, 86, 80, 73, 67, 61, 56}
+        DataLists(34) = {93, 86, 80, 74, 68, 62, 56}
+        DataLists(35) = {93, 87, 80, 74, 68, 63, 57}
+        DataLists(36) = {93, 87, 81, 75, 69, 63, 58}
+        DataLists(37) = {93, 87, 81, 75, 69, 64, 59}
+        DataLists(38) = {94, 87, 81, 75, 70, 64, 59}
+        DataLists(39) = {94, 87, 82, 76, 70, 65, 60}
+        DataLists(40) = {94, 88, 82, 76, 71, 66, 61}
+
+
+        dry = TextBox1.Text
+        wet = TextBox2.Text
+        diff = dry - wet
+
+        If diff < 7 Then
+            Label5.Text = diff
+            TextBox3.Text = DataLists(dry)(diff - 1)
+        Else
+            MessageBox.Show("ค่าผลต่างมากกกว่า 7 : ไม่พบข้อมูล", "Error")
+        End If
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
